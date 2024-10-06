8a57066475d65e58e55b5213f6c8ce840bab73d3
Pedro <psuar92@gmail.com>
2024-10-05 20:57:13 -0500

    styling done, functionality to be implemented

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,43 +1,83 @@
 import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material";
 import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formData = {
+    email: 'pedro@mail.com',
+    password: '123456',
+    displayName: 'Pedro Suarez'
+}
+
+const formValidations = {
+    email: [ (value) => value.includes('@'), 'Insert a valid email' ],
+    password: [ (value) => value.length >= 6, 'Password must have at least 6 characters' ],
+    displayName: [ (value) => value.length >= 1, 'The name is required' ],
+}
 
 export const RegisterPage = () => {
+
+    const {
+        formState, displayName, email, password, onInputChange,
+        isFormValid, displayNameValid, emailValid, passwordValid,
+    } = useForm(formData, formValidations);
+
+
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <AuthLayout title="Register">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid2 container>
                     <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
                         <TextField
+                            fullWidth
                             label="Full name"
                             type="text"
                             placeholder="Full name"
-                            fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
+                            error={!displayNameValid}
+                            helperText={displayNameValid}
                         />
                     </Grid2>
 
                     <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
                         <TextField
+                            fullWidth
                             label="Email"
                             type="email"
                             placeholder="email@example.com"
-                            fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid2>
 
                     <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
                         <TextField
+                            fullWidth
                             label="Password"
                             type="password"
                             placeholder="password"
-                            fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid2>
 
                     <Grid2 container size={12} spacing={2} sx={{ mb: 2, mt: 3 }}>
                         <Grid2 size={{ xs: 12 }}>
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                fullWidth
+                                variant="contained" 
+                                type="submit"
+                            >
                                 Create account
                             </Button>
                         </Grid2>
