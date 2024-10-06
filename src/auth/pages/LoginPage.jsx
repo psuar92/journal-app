0161069f5221aca094440f61d7bf9ace83f40d9d
Pedro <psuar92@gmail.com>
2024-10-05 20:54:03 -0500

    Implemented login with firebase

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,48 +1,91 @@
-import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material";
-import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
-import { AuthLayout } from "../layout/AuthLayout";
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid2, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { AuthLayout } from '../layout/AuthLayout';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from '../../hooks/';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
+import { useMemo } from 'react';
 
 export const LoginPage = () => {
+
+    const {status} = useSelector(state => state.auth);
+
+    const dispatch = useDispatch();
+
+    const {email, password, onInputChange} = useForm({
+        email: 'pedro@gmail.com',
+        password: '123456'
+    });
+
+    const isAuthenticating = useMemo(() => status === 'checking', [status]);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(checkingAuthentication());
+    }
+
+    const onGoogleSignIn = () => {
+        dispatch(startGoogleSignIn());
+    }
+
     return (
-        <AuthLayout title="Login">
-            <form>
+        <AuthLayout title='Login'>
+            <form onSubmit={onSubmit}>
                 <Grid2 container>
                     <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
                         <TextField
-                            label="Email"
-                            type="email"
-                            placeholder="email@example.com"
                             fullWidth
+                            label='Email'
+                            placeholder='email@example.com'
+                            type='email'
+                            name='email'
+                            value={email}
+                            onChange={onInputChange}
+                            autoComplete='off'
                         />
                     </Grid2>
 
                     <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
                         <TextField
-                            label="Password"
-                            type="password"
-                            placeholder="password"
                             fullWidth
+                            label='Password'
+                            type='password'
+                            placeholder='password'
+                            name='password'
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid2>
 
                     <Grid2 container size={12} spacing={2} sx={{ mb: 2, mt: 3 }}>
                         <Grid2 size={{ xs: 12, sm: 6 }}>
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                fullWidth
+                                type='submit' 
+                                variant='contained'
+                                disabled={isAuthenticating}
+                            >
                                 Login
                             </Button>
                         </Grid2>
                         <Grid2 size={{ xs: 12, sm: 6 }}>
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                fullWidth
+                                variant='contained' 
+                                onClick={onGoogleSignIn}
+                                disabled={isAuthenticating}
+                            >
                                 <Google />
                                 <Typography sx={{ ml: 1 }}>Google</Typography>
                             </Button>
                         </Grid2>
                     </Grid2>
 
-                    <Grid2 container size={12} direction="row" justifyContent="end">
+                    <Grid2 container size={12} direction='row' justifyContent='end'>
                     <Typography sx={{ mr: 1 }}>Don't have an account?</Typography>
-                        <Link component={RouterLink} color="inherit" to="/auth/register">
+                        <Link component={RouterLink} color='inherit' to='/auth/register'>
                             sign up
                         </Link>
                     </Grid2>
