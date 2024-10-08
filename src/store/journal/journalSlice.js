43ba6315f037baa294982f1602780a964d8a5f61
Pedro <psuar92@gmail.com>
2024-10-07 22:11:20 -0500

    add journalSlicer

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,47 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: false,
+        messageSaved: '',
+        notes: [],
+        activeNote: null,
+    },
+    reducers: {
+        addNewEmptyNote: (state, action) => {
+            state.notes.push(action.payload);
+            state.isSaving = false;
+        },
+        deleteNoteById: (state, action) => {
+            
+        },
+        savingNewNote: (state) => {
+            state.isSaving = true;
+        },
+        setActiveNote: (state, action) => {
+            state.activeNote = action.payload;
+        },
+        setNotes: (state, action) => {
+            state.notes = action.payload;
+        },
+        setSaving: (state) => {
+
+        },
+        updateNote: (state, action) => {
+
+        },
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const {
+    addNewEmptyNote,
+    deleteNoteById,
+    savingNewNote,
+    setActiveNote,
+    setNotes,
+    setSaving,
+    updateNote,
+} = journalSlice.actions;
