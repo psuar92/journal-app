b455803fedeac91cbd16b8a59d98de5b851b2814
Pedro <psuar92@gmail.com>
2024-10-07 22:08:41 -0500

    reduce code for better visibility

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,13 @@
-import { TurnedInNot } from "@mui/icons-material";
-import { Box, Divider, Drawer, Grid2, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, Grid2, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { SideBarItem } from './';
 
 export const SideBar = ({drawerWidth = 240}) => {
 
     const { displayName } = useSelector(state => state.auth);
+
+    const { notes } = useSelector(state => state.journal);
 
     return (
         <Box
@@ -29,18 +32,8 @@ export const SideBar = ({drawerWidth = 240}) => {
 
                 <List>
                     {
-                        ['January', 'February', 'March', 'April'].map( text => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid2 container>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={'Quis aliquip consectetur quis sit ex sunt.'} />
-                                    </Grid2>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map( note => (
+                            <SideBarItem key={note.id} {...note}/>
                         ))
                     }
                 </List>
