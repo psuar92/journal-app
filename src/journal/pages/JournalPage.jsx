8b76c63dbce2c8cdda65b40bbbb7c7e3dd76f118
Pedro <psuar92@gmail.com>
2024-10-07 22:10:13 -0500

    add button functionality and show/hide components when required

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,21 +2,40 @@ import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../view';
 import { AddOutlined } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { startNewNote } from '../../store/journal';
+import { useMemo } from 'react';
 
 export const JournalPage = () => {
+
+    const { isSaving, activeNote } = useSelector(state => state.journal);
+
+    const dispatch = useDispatch();
+
+    const checkSaving = useMemo(() => isSaving === true, [isSaving]);
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote());
+    }
+
     return (
         <JournalLayout>
 
-            <NothingSelectedView />
-
-            {/* <NoteView /> */}
+            {
+                (!!activeNote)
+                    ? <NoteView />
+                    : <NothingSelectedView />
+            }
 
             <IconButton
+                disabled={checkSaving}
+                onClick={onClickNewNote}
                 size='large'
                 sx={{
                     color: 'white',
                     backgroundColor: 'error.main',
                     ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    ':disabled': { backgroundColor: 'grey' },
                     position: 'fixed',
                     right: 50,
                     bottom: 50
