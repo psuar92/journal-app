77bd66c4b0fc3d8464bc7c26005841cb70127931
Pedro <psuar92@gmail.com>
2024-10-08 21:54:01 -0500

    update setSaving and updateNote reducers

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -22,15 +22,24 @@ export const journalSlice = createSlice({
         },
         setActiveNote: (state, action) => {
             state.activeNote = action.payload;
+            state.messageSaved = '';
         },
         setNotes: (state, action) => {
             state.notes = action.payload;
         },
         setSaving: (state) => {
-
+            state.isSaving = true;
+            state.messageSaved = '';
         },
         updateNote: (state, action) => {
+            state.isSaving = false;
+            state.notes = state.notes.map( note => {
+                
+                if( note.id === action.payload.id ) return action.payload;
 
+                return note;
+            });
+            state.messageSaved = `'${action.payload.title}' has been successfully updated`;
         },
     }
 });
