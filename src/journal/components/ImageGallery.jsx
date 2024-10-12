427a273cd76129b81df5060c36c8c02689d5c796
Pedro <psuar92@gmail.com>
2024-10-12 17:00:45 -0500

    fix error when creating new note

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -2,23 +2,17 @@ import { ImageList, ImageListItem } from "@mui/material";
 
 export const ImageGallery = ({ images }) => {
   return (
-    <>
-      {
-        images && images.length > 0 && (
-          <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
-            {images.map((image) => (
-              <ImageListItem key={image}>
-                <img
-                  srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                  src={`${image}?w=164&h=164&fit=crop&auto=format`}
-                  alt='Note image'
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        )}
-    </>
-  );
-
-}
+    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
+      {images.map((image) => (
+        <ImageListItem key={image}>
+          <img
+            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${image}?w=164&h=164&fit=crop&auto=format`}
+            alt='Note image'
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  )
+};
